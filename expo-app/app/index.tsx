d90e7666af6fe9b68b134df20aa98f7ba9f7c31b
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-10-31 14:06:55 +0200

    feat: changing values and logging them

--- expo-app/app/index.tsx
+++ expo-app/app/index.tsx
@@ -1,15 +1,51 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, Button } from "react-native";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
 
 export default function Index() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "stretch",
+        alignSelf: "stretch",
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <TextInput
+        defaultValue=""
+        value={getValues().example}
+        onChangeText={(newText) => {
+          setValue("example", newText);
+        }}
+        style={{
+          backgroundColor: "white",
+          borderColor: "black",
+          borderWidth: 2,
+        }}
+      />
+      <Button
+        title="Submit"
+        onPress={() => {
+          console.log(getValues());
+        }}
+      />
     </View>
   );
 }
